4095217b1163b5c785b86aa1a0f0e1ddb6836bc3
Zao Soula <contact@zaosoula.fr>
2020-11-29 02:29:49 +0100

    feat (server): Update local server specification

--- src/ui/dev/api/server.js
+++ src/ui/dev/api/server.js
@@ -3,6 +3,8 @@ const colors = require('colors'),
       path = require('path'),
       port = 3000;
 
+const VERSION = '0.2.2-alpha';
+
 let config = require('./defaultConf.json');
 
 colors.setTheme({
@@ -20,17 +22,12 @@ app.use(express.json())
 app.use('/', express.static(path.join(__dirname, '../../src/')));
 
 // Return the config
-app.get('/cfg.json', (req, res) => {
-  console.log('[GET /cfg.json] Config requested'.request);
-  res.json(config);
-  console.log(JSON.stringify(config).dataSent+"\n\n\n");
-})
-
-// To improve
-app.patch('/api/core', (req, res) => {
-  console.log('[PATCH /api/core] Update from github requested'.request);
-  console.log('This feature is not available in local development mode\n\n\n'.warn);
-  res.json(config)
+app.get(['/api/config', '/cfg.json'], (req, res) => {
+  setTimeout(()=>{
+    console.log('[GET /api/config] Config requested'.request);
+    res.json(config);
+    console.log(JSON.stringify(config).dataSent+"\n\n\n");
+  }, 500);
 })
 
 // Update config with new data (no validation) and return it
@@ -40,9 +37,32 @@ app.post('/api/config', (req, res) => {
   console.log(JSON.stringify(req.body).dataReceived);
   config = req.body;
   setTimeout(()=>{
+    console.log(JSON.stringify(config).dataSent+"\n\n\n");
     res.json(config);
   }, 500);
-  console.log(JSON.stringify(config).dataSent+"\n\n\n");
+})
+
+
+// Return the config
+app.get('/api/core/version', (req, res) => {
+  setTimeout(()=>{
+    console.log('[GET /api/core/version] Version requested'.request);
+    res.json(VERSION);
+    console.log(JSON.stringify(VERSION).dataSent+"\n\n\n");
+  }, 500);
+})
+
+// To improve
+app.post('/api/core/update', (req, res) => {
+  console.log('[POST /api/core/update] Update from github requested'.request);
+  console.log('This feature is not available in local development mode\n\n\n'.warn);
+  res.json(config);
+})
+
+app.put('/api/core/update', (req, res) => {
+  console.log('[PUT /api/core/update] Update from .bin file'.request);
+  console.log('This feature is not available in local development mode\n\n\n'.warn);
+  res.json(config);
 })
 
 app.listen(port, () => {
