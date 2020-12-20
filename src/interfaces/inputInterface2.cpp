d0fb141049fe1b72442bd7440fec50764b3ffab2
Simon J <jamain.simon@gmail.com>
2020-12-20 18:38:07 +0100

    bugfix on replica type 2 motot firing at startup

--- src/interfaces/inputInterface2.cpp
+++ src/interfaces/inputInterface2.cpp
@@ -27,7 +27,7 @@ void OMInputsInterface::begin()
   // motor pwm setup
   ledcAttachPin(motorPin, MOTOR_LEDC_CHANNEL);
   ledcSetup(MOTOR_LEDC_CHANNEL, MOTOR_LEDC_FREQ, MOTOR_LEDC_RES);
-  ledcWrite(MOTOR_LEDC_CHANNEL, 255); //turn off motor
+  ledcWrite(MOTOR_LEDC_CHANNEL, 0); //turn off motor
   
   pinMode(motorPin, OUTPUT);
 
